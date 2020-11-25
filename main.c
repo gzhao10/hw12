baa8e0754d2cb4cfd783ef4471025aa18f05eb81
gzhao10 <68718446+gzhao10@users.noreply.github.com>
2020-11-25 00:13:27 -0500

    Update main.c

--- main.c
+++ main.c
@@ -4,18 +4,36 @@
 #include <dirent.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <errno.h>
 
-int main(){
+int main(int argc, char ** argv){
+  char buffer[256];
   DIR * d;
-  d = opendir("./");
+
+  if (argc > 1){
+    strcpy(buffer, argv[1]);
+  }
+
+  else{
+    printf("Please enter a directory: ");
+    scanf("%s", buffer);
+  }
+
+  d = opendir(buffer);
+
+  if (d == NULL) {
+    printf("%s\n", strerror(errno));
+    return 0;
+  }
+
   struct dirent *entry;
   entry = readdir(d);
   int dirtype = entry->d_type;
 
   struct stat sb;
-  stat("./", &sb);
+  stat(buffer, &sb);
 
-  printf("Statistics for directory: %s\n", entry->d_name);
+  printf("Statistics for directory: %s\n", buffer);
   printf("Total Directory Size: %lld Bytes\n", sb.st_size);
   printf("Directories:\n");
 
